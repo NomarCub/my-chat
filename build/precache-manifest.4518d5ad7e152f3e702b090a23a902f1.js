b4d92248ca4839065a4f57d553cf95c2921e8a17
NomarCub <nomarcub@gmail.com>
2020-05-17 11:02:05 +0200

    5.2 Release build

---
+++ build/precache-manifest.4518d5ad7e152f3e702b090a23a902f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82c7e08cfee24924551c2453b486a84a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "79ab44865849087dfbfb",
+    "url": "./static/css/main.dad4a93c.chunk.css"
+  },
+  {
+    "revision": "5ed28e986c9d3ccfaaa3",
+    "url": "./static/js/2.0f4066fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0f4066fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79ab44865849087dfbfb",
+    "url": "./static/js/main.628be7e0.chunk.js"
+  },
+  {
+    "revision": "3b5176513c78f0fdd135",
+    "url": "./static/js/runtime-main.03defe92.js"
+  }
+]);
